6ce8b8dafbbe6f0e94b25e6ffd5726bb955350f4
Solirix <hades5132@gmail.com>
2022-12-07 14:12:18 -0500

    an array... I guess??

--- main.cpp
+++ main.cpp
@@ -5,9 +5,23 @@
 // This program does a breadth-first search of a graph.
 
 #include <iostream>
+#include <string>
+
+#define ROWS 6
+#define COLS 6
 
 int main(void){
+    // create a 2D graph of names and their connections
+    int graph[ROWS][COLS] = {
+        {1, 0, 0, 1, 1, 0},
+        {0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0}
+    };
 
+    char VertexNames[6] = {'A', 'B', 'C', 'D', 'E', 'F'};
 
 return 0;
 }
