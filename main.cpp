10e383efaa7e3ddf8fe52ab2834c6ba644dba158
Solirix <hades5132@gmail.com>
2022-12-08 15:12:52 -0500

    oops

--- main.cpp
+++ main.cpp
@@ -10,7 +10,11 @@
 #define ROWS 7
 #define COLS 7
 
+void breadthFirstSearch(int graph[ROWS][COLS], std::string vertexNames[7], int startVertex);
+
 int main(void){
+    int userInput = 0;
+
     // create a 2D graph of names and their connections
     int graph[ROWS][COLS] = {
         {0, 1, 1, 0, 0, 0, 0},
@@ -22,16 +26,24 @@ int main(void){
         {0, 0, 0, 0, 0, 1, 0}
     };
 
-    std::string VertexNames[7] = {"joe", "eva", "taj", "chen", "lily", "jun", "ken"};
+    std::string vertexNames[7] = {"joe", "eva", "taj", "chen", "lily", "jun", "ken"};
+
+
+
+
+
 
     // print the graph
-    std::cout << "Graph:" << std::endl;
-    for(int i = 0; i < ROWS; i++){
-        for(int j = 0; j < COLS; j++){
-            std::cout << graph[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
+    // std::cout << "Graph:" << std::endl;
+    // for(int i = 0; i < ROWS; i++){
+    //     for(int j = 0; j < COLS; j++){
+    //         std::cout << graph[i][j] << " ";
+    //     }
+    //     std::cout << std::endl;
+    // }
 
 return 0;
 }
+
+// create a breadth first search with the linked list
+
